7410143f0e9046d4a600159405a5b5603fb730d2
Soham <sohamadhyapak0005@gmail.com>
2023-03-11 20:04:35 +0530

    Click Share

---
+++ clickShare/home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+# Register your models here.
+from .models import Folder,Files
+
+
+admin.site.register(Files)
+admin.site.register(Folder)
---
+++ clickShare/home/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+
+
+
+
+def home(request):
+    return render(request,'home.html')
+
+
+
+
+
+
+
+class Handle_Uploaded_Files(APIView):
+    parser_classes = [MultiPartParser]
+    def post(self , request):
+        try:
+            data = request.data
+
+            serializer = FileListSerializer(data = data)
+        
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status' : 200,
+                    'message' : 'files uploaded successfully',
+                    'data' : serializer.data
+                })
+            
+            return Response({
+                'status' : 400,
+                'message' : 'somethign went wrong',
+                'data'  : serializer.errors
+            })
+        except Exception as e:
+            print(e)
+
+
+        
+    
+    # def get(self,request):
+        
+    #     try:
+    #         # print(data)
+    #         return Response({
+    #             "status":200,
+    #             "message":"success"
+    #         })
+    #     except Exception as e:
+    #         print(e)
+                
+            
+            
