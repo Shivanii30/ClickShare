11d3ff92d4058c6d7075b39e5991b758902100a0
Soham <soham0005>
2023-05-07 22:59:47 +0530

    Download Page UI and Button Added

--- clickShare/clickShare/settings.py
+++ clickShare/clickShare/settings.py
@@ -167,5 +167,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS= True
 EMAIL_PORT= 587
-EMAIL_HOST_USER= 'sohamadhyapak0005@gmail.com'
-EMAIL_HOST_PASSWORD='vybcncmtcnurnuof'
+EMAIL_HOST_USER= 'kshirsagarshivani1438@gmail.com'
+EMAIL_HOST_PASSWORD='chsvpqcinystmilq'
