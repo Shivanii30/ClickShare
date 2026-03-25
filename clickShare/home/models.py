0902bfef9221b09c8b9f668c1d578ddd0b98d05d
Shivani Kshirsagar <108947015+Shivanii30@users.noreply.github.com>
2026-03-25 22:33:55 +0530

    add new fields

--- clickShare/home/models.py
+++ clickShare/home/models.py
@@ -1,20 +1,25 @@
-from pyexpat import model
-from statistics import mode
-from uuid import uuid4
-from django.db import models
-import uuid
 import os
+import uuid
+
+from django.db import models
+from django.utils import timezone
+
 
 class Folder(models.Model):
-    uid = models.UUIDField(primary_key= True , editable= False , default=uuid.uuid4)
-    created_at = models.DateField(auto_now= True)
- 
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    zip_public_id = models.CharField(max_length=255, blank=True)
+    zip_url = models.URLField(blank=True)
+    original_file_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
 
-def get_upload_path(instance , filename):
-    return os.path.join(str(instance.folder.uid) , filename)
+
+def get_upload_path(instance, filename):
+    safe_name = os.path.basename(filename)
+    return os.path.join(str(instance.folder.uid), safe_name)
 
 
 class Files(models.Model):
-    folder = models.ForeignKey(Folder , on_delete=models.CASCADE)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name="files")
     file = models.FileField(upload_to=get_upload_path)
-    created_at = models.DateField(auto_now= True)
+    original_name = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
